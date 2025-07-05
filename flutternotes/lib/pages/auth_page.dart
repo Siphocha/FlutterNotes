56c8ff0f1d580c03c3f1f255b8188efba53690b9
Siphocha <siphochakhala@gmail.com>
2025-07-05 18:21:02 +0200

    adding auth page but making it part of pages as its not direct auth

---
+++ flutternotes/lib/pages/auth_page.dart
@@ -0,0 +1,26 @@
+// auth_page.dart
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../authLogic/auth_bloc.dart';
+import '../widgets/auth_forms.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Notes App')),
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state.status == AuthStatus.error) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.errorMessage ?? 'Unknown error')),
+            );
+          }
+        },
+        child: const Padding(padding: EdgeInsets.all(16.0), child: AuthForm()),
+      ),
+    );
+  }
+}
